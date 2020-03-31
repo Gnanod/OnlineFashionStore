e6e1c044e7947d6e163af6c7d37a3a39ca6d546c
gnanod <1995gnanod@gmail.com>
2020-04-01 00:21:46 +0530

    Complete Item Management UI

--- FashionStore/adminfrontend/src/components/Views/Item/BrandCategory.js
+++ FashionStore/adminfrontend/src/components/Views/Item/BrandCategory.js
@@ -1,13 +1,109 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
 import './Style/Item.css'
+import {
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardText,
+    MDBCardTitle,
+    MDBCol,
+    MDBInput,
+    MDBRow, MDBTable,
+    MDBTableBody, MDBTableHead
+} from "mdbreact";
+import {AutoComplete} from "rsuite";
 
-export default  class BrandCategory extends Component{
+export default class BrandCategory extends Component {
 
-    render(){
+    render() {
 
-        return(
+        return (
 
             <div className=" container-fluid BrandCategoryMain">
+
+                <br/>
+                <br/>
+                <MDBRow>
+                    <MDBCol size="6">
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBCardTitle>Add New Brand</MDBCardTitle>
+                                <form>
+                                    <MDBInput label="Brand Name" size="sm"/>
+                                    <MDBInput label="Brand Code" size="sm"/>
+                                    <MDBInput label="Selling Discount" size="sm"/>
+                                    <MDBBtn href="#">Save</MDBBtn>
+                                </form>
+                            </MDBCardBody>
+                        </MDBCard>
+
+                    </MDBCol>
+
+                    <MDBCol size="6">
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBCardTitle>Add New Category</MDBCardTitle>
+                                <form>
+                                    <MDBInput label="Category Name" size="sm"/>
+                                    <MDBInput label="Category Code" size="sm"/>
+                                    <MDBBtn href="#">Save</MDBBtn>
+                                </form>
+                            </MDBCardBody>
+                        </MDBCard>
+
+                    </MDBCol>
+                </MDBRow>
+
+                <br/>
+                <MDBRow>
+
+                    <MDBCol>
+
+                        <MDBCard size="6">
+                            <MDBCardBody>
+                                <MDBCardTitle>Add New Category</MDBCardTitle>
+                                <form>
+                                    <AutoComplete data={''} placeholder="BrandName"/>
+                                    <br/>
+                                    <AutoComplete data={''} placeholder="Category Name"/>
+                                    <br/>
+                                    <MDBBtn href="#">Add</MDBBtn>
+
+
+                                <br/>
+                                <br/>
+                                <MDBTable>
+                                    <MDBTableHead color="primary-color" textWhite>
+                                        <tr>
+                                            <th>Brand Name</th>
+                                            <th>Category Name</th>
+
+                                        </tr>
+                                    </MDBTableHead>
+                                    <MDBTableBody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Mark</td>
+
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Jacob</td>
+
+                                        </tr>
+
+                                    </MDBTableBody>
+                                </MDBTable>
+                                <MDBBtn href="#">Save</MDBBtn>
+                            </form>
+
+                            </MDBCardBody>
+                        </MDBCard>
+
+
+                    </MDBCol>
+                    <MDBCol size="6"/>
+                </MDBRow>
 
             </div>
         );
--- FashionStore/adminfrontend/src/components/Views/Item/ItemColor.js
+++ FashionStore/adminfrontend/src/components/Views/Item/ItemColor.js
@@ -1,14 +1,104 @@
 import React,{Component} from "react";
 import './Style/Item.css'
+import {
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCol,
+    MDBInput,
+    MDBRow,
+    MDBTable,
+    MDBTableBody,
+    MDBTableHead
+} from "mdbreact";
 
+import {PhotoshopPicker, SketchPicker, SwatchesPicker} from 'react-color';
 export default  class ItemColor extends Component{
 
+    state = {
+        background: '#fff',
+    };
+
+   ;
+
+    handleChange(color, event) {
+        // color = {
+        //   hex: '#333',
+        //   rgb: {
+        //     r: 51,
+        //     g: 51,
+        //     b: 51,
+        //     a: 1,
+        //   },
+        //   hsl: {
+        //     h: 0,
+        //     s: 0,
+        //     l: .20,
+        //     a: 1,
+        //   },
+        // }
+
+        console.log(color.hex)
+    }
     render(){
 
         return(
 
             <div className=" container-fluid itemColorMain">
+                <br/>
+                <br/>
+                <MDBRow>
+                    <MDBCol size="8">
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBCardTitle>Add New Item</MDBCardTitle>
+                                <form>
+                                    <MDBInput label="Item Code" size="sm"/>
+                                    <MDBInput label="Item Size" size="sm"/>
+                                    <SwatchesPicker onChange={ this.handleChange } />
 
+                                    <MDBBtn href="#">Save</MDBBtn>
+
+                                    <br/>
+                                    <br/>
+                                    <MDBTable>
+                                        <MDBTableHead color="primary-color" textWhite>
+                                            <tr>
+                                                <th>Item Id</th>
+                                                <th>Color Id</th>
+                                                <th>Size</th>
+
+                                            </tr>
+                                        </MDBTableHead>
+                                        <MDBTableBody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Mark</td>
+                                                <td>Mark</td>
+
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td>Jacob</td>
+                                                <td>Jacob</td>
+
+                                            </tr>
+
+                                        </MDBTableBody>
+                                    </MDBTable>
+                                    <MDBBtn href="#">Save</MDBBtn>
+                                </form>
+                            </MDBCardBody>
+                        </MDBCard>
+
+                    </MDBCol>
+
+                    <MDBCol size="4">
+
+
+                    </MDBCol>
+                </MDBRow>
             </div>
         );
     }
