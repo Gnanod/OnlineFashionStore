e100760801bd65a91236f0a7dffbaedaf218b666
gnanod <1995gnanod@gmail.com>
2020-03-31 13:13:55 +0530

    reomove background color css in mainwidth class

--- FashionStore/adminfrontend/src/components/SideNavigationbar/SideNavigationbar.js
+++ FashionStore/adminfrontend/src/components/SideNavigationbar/SideNavigationbar.js
@@ -11,47 +11,74 @@ export default class SideNavigationbar extends Component {
     render() {
         return (
 
-                    <Sidenav defaultOpenKeys={['3', '4']} activeKey="1">
-                        <Sidenav.Body className="navBarHeight">
-                            <Nav>
-                                <Nav.Item eventKey="1" icon={<Icon icon="dashboard"/>}>
-                                    <Link to='/' > Dashboard </Link>
-                                </Nav.Item>
-                                <Dropdown eventKey="2" title="User Management" icon={<Icon icon="group"/>}>
+            <Sidenav defaultOpenKeys={['3', '4']} activeKey="1">
+                <Sidenav.Body className="navBarHeight">
+                    <Nav>
+                        <Nav.Item eventKey="1" icon={<Icon icon="dashboard"/>}>
+                            <Link to='/'> Dashboard </Link>
+                        </Nav.Item>
+                        <Dropdown eventKey="2" title="User Management" icon={<Icon icon="group"/>}>
 
-                                    <Dropdown.Item eventKey="2-2">Devices</Dropdown.Item>
-                                    <Dropdown.Item eventKey="2-3">Loyalty</Dropdown.Item>
-                                    <Dropdown.Item eventKey="2-4">Visit Depth</Dropdown.Item>
-                                </Dropdown>
-                                <Dropdown eventKey="3" title="Item Management" icon={<Icon icon="gear-circle"/>}>
-                                    <Dropdown.Item eventKey="3-3">
-                                        <Link to='/item/brandcategory' > Brand & Category </Link>
+                            <Dropdown.Item eventKey="2-4">
+                                <Link to='/item/brandcategory'> Brand & Category </Link>
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey="2-3">Loyalty</Dropdown.Item>
+                            <Dropdown.Item eventKey="2-4">Visit Depth</Dropdown.Item>
+                        </Dropdown>
+                        <Dropdown  title="Item Management" icon={<Icon icon="gear-circle"/>}>
 
-                                    </Dropdown.Item>
-                                    <Dropdown.Item eventKey="3-1">
-                                        <Link to='/item/newitem' > New Item </Link>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item eventKey="3-3">
-                                        <Link to='/item/itemcolor' > Item Color </Link>
+                            <Dropdown.Item >
+                                <Link to='/item/brandcategory'> Brand & Category </Link>
+                            </Dropdown.Item>
 
-                                    </Dropdown.Item>
+                            <Dropdown.Item >
+                            <Link to='/item/newitem'> New Item </Link>
+                            </Dropdown.Item>
+                            <Dropdown.Item >
+                            <Link to='/item/itemcolor'> Item Color </Link>
 
-                                    <Dropdown.Item eventKey="3-2">
-                                        <Link to='/item/SearchItem' > Search Item </Link>
-                                    </Dropdown.Item>
+                            </Dropdown.Item>
 
-                                    <Dropdown.Menu eventKey="3-5" title="Custom Action">
-                                        <Dropdown.Item eventKey="3-5-1">Action Name</Dropdown.Item>
-                                        <Dropdown.Item eventKey="3-5-2">Action Params</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                                <Nav.Item eventKey="1" icon={<Icon icon="dashboard"/>}>
-                                    <Link to='/logout' > LogOut </Link>
-                                </Nav.Item>
-                            </Nav>
-                        </Sidenav.Body>
-                    </Sidenav>
+                            <Dropdown.Item >
+                            <Link to='/item/SearchItem'> Search Item </Link>
+                            </Dropdown.Item>
+                        </Dropdown>
 
+                        {/*<Dropdown eventKey="4" title="Item Management" icon={<Icon icon="gear-circle"/>}>*/}
+                            {/*<Dropdown.Item eventKey="4-2">*/}
+                                {/*<Link to='/item/brandcategory'> Brand & Category </Link>*/}
+                            {/*</Dropdown.Item>*/}
+
+                        {/*</Dropdown>*/}
+
+                        {/*<Dropdown eventKey="3" title="Item Management" icon={<Icon icon="gear-circle"/>}>*/}
+                            {/*<Dropdown.Item eventKey="3-1">*/}
+                                {/*<Link to='/item/brandcategory'> Brand & Category </Link>*/}
+
+                            {/*</Dropdown.Item>*/}
+                            {/*<Dropdown.Item eventKey="3-2">*/}
+                                {/*<Link to='/item/newitem'> New Item </Link>*/}
+                            {/*</Dropdown.Item>*/}
+                            {/*<Dropdown.Item eventKey="3-3">*/}
+                                {/*<Link to='/item/itemcolor'> Item Color </Link>*/}
+
+                            {/*</Dropdown.Item>*/}
+
+                            {/*<Dropdown.Item eventKey="3-5">*/}
+                                {/*<Link to='/item/SearchItem'> Search Item </Link>*/}
+                            {/*</Dropdown.Item>*/}
+
+                            {/*<Dropdown.Menu eventKey="3-6" title="Custom Action">*/}
+                                {/*<Dropdown.Item eventKey="3-6-1">Action Name</Dropdown.Item>*/}
+                                {/*<Dropdown.Item eventKey="3-6-2">Action Params</Dropdown.Item>*/}
+                            {/*</Dropdown.Menu>*/}
+                        {/*</Dropdown>*/}
+                        <Nav.Item eventKey="4" icon={<Icon icon="dashboard"/>}>
+                            <Link to='/logout'> LogOut </Link>
+                        </Nav.Item>
+                    </Nav>
+                </Sidenav.Body>
+            </Sidenav>
 
 
         );
