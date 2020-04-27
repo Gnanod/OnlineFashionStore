3efb6a3487cddce038c62936c0459c64db223557
Samitha-Vidhanaarachchi <samithavidhanaarachchi@gmail.com>
2020-04-28 01:47:30 +0530

    client frontend addition

--- FashionStore/ClientFrontend/src/components/Navigationbar/Navigationbar.js
+++ FashionStore/ClientFrontend/src/components/Navigationbar/Navigationbar.js
@@ -30,7 +30,10 @@ export default class Navigationbar extends Component {
             isWideEnough: false,
             modal: false,
             model2 : false,
-
+            fname: "",
+            lname: "",
+            email: "",
+            gender: ""
         };
         this.onClick = this.onClick.bind(this);
     }
@@ -56,9 +59,24 @@ export default class Navigationbar extends Component {
 
     toggle3 = () => {
         this.setState({
-            modal2: false
+            modal2: false,
+            fname: "",
+            lname: "",
+            email: "",
+            gender: ""
+
         });
     }
+
+    submitHandler = event => {
+        event.preventDefault();
+        event.target.className += " was-validated";
+    };
+
+    changeHandler = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
 
     render() {
         return (
@@ -94,7 +112,7 @@ export default class Navigationbar extends Component {
                                 </MDBNavbarNav>
                                 <MDBNavbarNav right>
                                     <MDBNavItem>
-                                        <MDBBtn color="dark" onClick={this.toggle}>Login</MDBBtn>
+                                        <MDBBtn color="dark" onClick={this.toggle}> <i className="fas fa-user"></i>&nbsp;&nbsp;&nbsp;Login</MDBBtn>
                                     </MDBNavItem>
                                 </MDBNavbarNav>
                             </MDBCollapse>
@@ -156,13 +174,49 @@ export default class Navigationbar extends Component {
                                                 <strong className="loginh3 ">REGISTER</strong>
                                             </h2>
                                         </div>
-                                        <MDBInput label="Your email" group type="email" validate error="wrong" success="right"/>
-                                        <MDBInput label="Your password" group type="password" validate containerClass="mb-0"/>
-                                        <div className="text-center mb-3">
-                                            <MDBBtn type="button" gradient="blue" rounded className="btn-block z-depth-1a">
+                                        {/*<MDBInput label="Your email" group type="email" validate error="wrong" success="right"/>*/}
+                                        {/*<MDBInput label="Your password" group type="password" validate containerClass="mb-0"/>*/}
+
+                                        <form className="needs-validation" onSubmit={this.submitHandler} noValidate>
+                                            <MDBRow>
+                                                {/*<MDBCol  md="12" className="mb-3">*/}
+                                                    <label htmlFor="defaultFormRegisterNameEx" className="grey-text">First name</label>
+                                                    <input value={this.state.fname} name="fname" onChange={this.changeHandler} type="text" id="defaultFormRegisterNameEx" className="form-control" placeholder="First name" required/>
+                                                <div className="invalid-feedback">Please provide the first name.</div>
+
+                                                {/*</MDBCol>*/}
+                                                {/*<MDBCol  md="12" className="mb-3">*/}
+                                                    <label htmlFor="defaultFormRegisterEmailEx2" className="grey-text">Last name</label>
+                                                    <input value={this.state.lname} name="lname" onChange={this.changeHandler} type="text" id="defaultFormRegisterEmailEx2" className="form-control" placeholder="Last name" required/>
+                                                <div className="invalid-feedback">Please provide the last name.</div>
+
+                                                {/*</MDBCol>*/}
+                                                {/*<MDBCol md="4" className="mb-3">*/}
+                                                    <label htmlFor="defaultFormRegisterConfirmEx3" className="grey-text">Email</label>
+                                                    <input value={this.state.email} onChange={this.changeHandler} type="email" id="defaultFormRegisterConfirmEx3" className="form-control" name="email" placeholder="Your Email address"/>
+                                                <div className="invalid-feedback">Please provide an email.</div>
+
+                                                {/*</MDBCol>*/}
+                                            </MDBRow>
+                                            <MDBRow>
+                                                {/*<MDBCol md="6" className="mb-0">*/}
+                                                    <label htmlFor="defaultFormRegisterPasswordEx4" className="grey-text">Date of Birth</label>
+                                                    <input onChange={this.changeHandler} type="date" id="defaultFormRegisterPasswordEx4" className="form-control" name="DOB" placeholder="Date of Birth" required/>
+                                                    <div className="invalid-feedback">Please provide DOB</div>
+
+                                                {/*</MDBCol>*/}
+                                                {/*<MDBCol md="6" className="mb-0">*/}
+                                                    <label htmlFor="defaultFormRegisterPasswordEx4" className="grey-text">State</label>
+                                                    <input value={this.state.gender} onChange={this.changeHandler} type="text" id="defaultFormRegisterPasswordEx4" className="form-control" name="gender" placeholder="Gender" required/>
+                                                    <div className="invalid-feedback">Please provide your gender.</div>
+
+                                                {/*</MDBCol>*/}
+                                            </MDBRow>
+                                            <br></br>
+                                            <MDBBtn  gradient="blue" rounded className="btn-block z-depth-1a" type="submit">
                                                 REGISTER
                                             </MDBBtn>
-                                        </div>
+                                        </form>
                                     </MDBCardBody>
                                 </MDBCard>
                             </MDBModalBody>
