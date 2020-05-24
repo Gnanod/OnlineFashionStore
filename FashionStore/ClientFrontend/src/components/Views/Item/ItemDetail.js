7ed5eeeeb8aa80ad0a0b55ef90246945dde93a8c
divyani123 <divyanirajapaksha@gmail.com>
2020-05-24 18:32:58 +0530

    dis

--- FashionStore/ClientFrontend/src/components/Views/Item/ItemDetail.js
+++ FashionStore/ClientFrontend/src/components/Views/Item/ItemDetail.js
@@ -228,7 +228,7 @@ export class ItemDetail extends Component {
                     quantity:1,
                     itemTotal:cartItem.itemSizes.price,
                     itemId:cartItem.itemSizes._id,
-                    itemDiscount:100,
+                    itemDiscount:this.state.discount,
                     itemSize:cartItem.itemSizes.itemSize
                 }
                 this.decrementQuantity(cartItem.itemSizes._id,cartItem.itemSizes.quantity);
